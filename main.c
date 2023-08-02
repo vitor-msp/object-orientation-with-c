0c630dcd1f898cf59eb3b98190ccbc340e290754
Vitor Mateus <vitor7jan@gmail.com>
2023-08-02 19:31:29 -0300

    inheritance

--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 
 typedef struct cart_t
 {
-    int products[MAX_PRODUCTS];
-    int totalValue;
+    float products[MAX_PRODUCTS];
+    float totalValue;
     void (*sumValues)(void *);
 } Cart;
 
 void sumValues(void *this)
 {
-    int tmpSum = 0;
+    float tmpSum = 0;
     for (int i = 0; i < MAX_PRODUCTS; i++)
     {
         tmpSum += ((Cart *)this)->products[i];
@@ -24,7 +24,34 @@ Cart cartConstructor(void)
 {
     Cart cart;
     cart.sumValues = &sumValues;
+    cart.products[0] = 0;
+    cart.products[1] = 0;
+    cart.totalValue = 0;
     return cart;
+}
+
+typedef struct vip_cart_t
+{
+    Cart super;
+    float discount;
+    void (*applyDiscount)(void *);
+} VipCart;
+
+void applyDiscount(void *this)
+{
+    float oldValue = ((VipCart *)this)->super.totalValue;
+    float discount = ((VipCart *)this)->discount;
+    float newValue = oldValue * (1 - discount);
+    ((VipCart *)this)->super.totalValue = newValue;
+}
+
+VipCart vipCartConstructor()
+{
+    VipCart vipCart;
+    vipCart.super = cartConstructor();
+    vipCart.discount = 0;
+    vipCart.applyDiscount = &applyDiscount;
+    return vipCart;
 }
 
 void main()
@@ -33,5 +60,13 @@ void main()
     cart.products[0] = 10;
     cart.products[1] = 25;
     cart.sumValues(&cart);
-    printf("total value: %d\n", cart.totalValue);
+    printf("cart - total value: %f\n", cart.totalValue);
+
+    VipCart vipCart = vipCartConstructor();
+    vipCart.super.products[0] = 10;
+    vipCart.super.products[1] = 40;
+    vipCart.super.sumValues(&vipCart);
+    vipCart.discount = 0.2;
+    vipCart.applyDiscount(&vipCart);
+    printf("vip cart - total value: %f\n", vipCart.super.totalValue);
 }
