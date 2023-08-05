e440770b6eccce5953cae8aa938067df5de40e9a
Vitor Mateus <vitor7jan@gmail.com>
2023-08-05 18:15:04 -0300

    implemented check by stack to ensure encapsulation

--- main.c
+++ main.c
@@ -1,22 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+#include <execinfo.h>
+#include <unistd.h>
+#include "analyze-stack.h"
 
 #define MAX_PRODUCTS 2
 #define MAX_LOG_MESSAGE 20
+#define BUFFER_STACK_SIZE 1000
+#define READ 0
+#define WRITE 1
+#define FUNCTIONS_QTT 2
 
+int pipe_fd[2];
+
+int validateCall()
+{
+    char buffer[BUFFER_STACK_SIZE];
+    alarm(1);
+    pause();
+    read(pipe_fd[READ], buffer, BUFFER_STACK_SIZE);
+    char *functionIsValid = NULL;
+    char validFunctions[FUNCTIONS_QTT][BUFFER_STACK_SIZE] = {"sumValues", "applyDiscount"};
+    for (int i = 0; i < FUNCTIONS_QTT; i++)
+    {
+        functionIsValid = strstr(buffer, validFunctions[i]);
+        if (functionIsValid)
+            return 1;
+    }
+    return 0;
+}
+
+/////////////////// ENCAPSULATED VARIABLE ///////////////////
 float totalValue;
-
 float getTotalValue()
 {
     return totalValue;
 }
-
 void setTotalValue(float newTotalValue)
 {
-    // validation by process stack
-    totalValue = newTotalValue;
+    if (validateCall())
+        totalValue = newTotalValue;
 }
+/////////////////// ENCAPSULATED VARIABLE ///////////////////
 
 typedef struct cart_t
 {
@@ -132,21 +159,30 @@ struct ILogger loggerConstructor2()
     return logger;
 }
 
-void main()
+void main(int argc, char *argv[])
 {
+    pipe(pipe_fd);
+    set_user_fd(pipe_fd[WRITE]);
+    analyze_stack(argv[0]);
+
     // Cart cart = cartConstructor();
     // cart.products[0] = 10;
     // cart.products[1] = 25;
     // cart.sumValues(&cart);
     // printf("cart - total value: %f\n", cart.totalValue);
 
+    setTotalValue(-1);
+    printf("total value (-1): %f\n", totalValue);
     struct ILogger logger1 = loggerConstructor1();
     VipCart vipCart = vipCartConstructor();
     vipCart.super.products[0] = 10;
     vipCart.super.products[1] = 40;
     vipCart.discount = 0.2;
     vipCart.sumValues(&vipCart, logger1);
-    printf("vip cart - total value: %f\n", vipCart.super.getTotalValue());
+    printf("vip cart - total value (40): %f\n", vipCart.super.getTotalValue());
+    printf("total value (40): %f\n", totalValue);
+    setTotalValue(0);
+    printf("total value (0): %f\n", totalValue);
 
     // struct ILogger logger2 = loggerConstructor2();
     // VipCart polyVipCart = polymorphicVipCartConstructor();
@@ -156,4 +192,7 @@ void main()
     // polyVipCart.discount = 0.5;
     // polyVipCart.applyDiscount(&polyVipCart, logger2);
     // printf("poly vip cart - total value: %f\n", polyVipCart.super.totalValue);
+
+    close(pipe_fd[READ]);
+    close(pipe_fd[WRITE]);
 }
